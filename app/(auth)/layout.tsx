9b0d9596b6fc538a46c2a66f4edb0e0556daa767
Solideo Putra Zendrato <solidzendrato@gmail.com>
2024-07-30 18:34:46 +0200

    initial installation

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import { Separator } from "@/components/ui/separator"
+
+const AuthLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-50 to-white">
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default AuthLayout
